de617034081732f4c2f25b0100790e52bdad0ff5
Mncedisi Masondo <mncedisimasondo4@gmail.com>
2024-11-09 22:37:10 +0200

    added new app routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:'login', component: SignInComponent},
+    {path: '', redirectTo:'/login', pathMatch:'full'}
+];
