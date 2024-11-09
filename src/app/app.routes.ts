8facfdf51a43dbd0eb1f4fd3559dcfbcecdaa2cd
Mncedisi Masondo <mncedisimasondo4@gmail.com>
2024-11-10 01:13:04 +0200

    Modified add product component  -added css style, html and functions in ts file. Added new routes in app routes and modified navbar component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from '@angular/router';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { IndexComponent } from './components/index/index.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { EditProductComponent } from './components/edit-product/edit-product.component';
+import { UsersComponent } from './components/users/users.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 
 export const routes: Routes = [
 
     {path:'login', component: SignInComponent},
-    {path:'index', component: IndexComponent},
+    {path:'products', component: IndexComponent},
+    {path:'productdetail', component: ProductDetailComponent},
+    {path:'addproduct', component: AddProductComponent},
+    {path:'editproduct', component: EditProductComponent},
+    {path:'users', component:UsersComponent},
     {path:'', redirectTo:'/login', pathMatch:'full'}
 ];
--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -1,12 +1,90 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ProductService } from '../../services/product.service';
+
+/**
+ * @author Mncedisi Masondo
+ * 
+ * @description Component Responsible for adding new product to fakestore api
+ */
 
 @Component({
   selector: 'app-add-product',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,NgIf],
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.css'
 })
 export class AddProductComponent {
+
+  // declaration of properties
+  productForm : FormGroup
+  showSuccessMessage: boolean = false; showErrorMessage : boolean = false
+
+  /**
+  * @param formBuilder 
+  */
+  constructor(private formBuilder: FormBuilder,private productService:ProductService){
+    // Initialization of product form
+    this.productForm = this.formBuilder.group({
+      //declaration of properties
+      title: ['', [Validators.required]],
+      category: ['', [Validators.required]],
+      price: ['',[Validators.required]],
+      image: ['', Validators.required],
+      description: ['', [Validators.required]]
+    })
+  }
+
+  /**
+   * @description Helper function that add new product to fakestore api
+   */
+  addProduct(){
+    // create new product
+    const newProduct = {
+      title: this.productForm.get('title')?.value,
+      price: this.productForm.get('price')?.value,
+      description: this.productForm.get('description')?.value,
+      image: this.productForm.get('image')?.value,
+      category: this.productForm.get('category')?.value
+    }
+
+    // call the helper function from fakestore api to add new product
+    this.productService.addProduct(newProduct).subscribe({
+      next: response => {
+        // print out message in the user, setout time
+        console.log(response);
+        
+        setTimeout(() => {
+          // reset form 
+          this.productForm.reset()
+          // display message
+          this.showSuccessMessage = true
+          // hide message
+          setTimeout(() => {
+            // hide message
+            this.showSuccessMessage = false
+
+          }, 2500)
+        }, 1500);
+         
+      },
+      error: error => {
+        // display error message
+        setTimeout(() => {
+          // reset form
+          this.productForm.reset()
+          // show error message
+          this.showErrorMessage = true
+          // hide message
+          setTimeout(() => {
+            this.showErrorMessage = false
+          },2500)
+        },1500)
+      }
+    })
+  }
+
 
 }
