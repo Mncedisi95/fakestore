1d877255997b4c293a9816693bb587d351a59507
Mncedisi Masondo <mncedisimasondo4@gmail.com>
2024-11-10 01:56:06 +0200

    modified navbar component and product detail component -added css style html and functionality in ts file.

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,12 +1,96 @@
+import { CurrencyPipe, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+
+/**
+ * @author Mncedisi Masondo
+ * 
+ * @description Component Responsible for displaying single product details from fakestore api
+ */
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [NgIf,CurrencyPipe],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent {
+  
+   // declaration of properties
+   product : any | undefined
+   productId : number = 0
+   showSuccessMessage : boolean = false; showFailedMessage : boolean = false
+ 
+   /**
+    * @param activatedRoute 
+    * @param productService 
+    */
+   constructor(private router: Router,private activatedRoute:ActivatedRoute,private productService:ProductService){}
+ 
+     /**
+   * @description initializes the component after angular fist display the data-bound properties
+   * 
+   * Fetch product data from the fakestore api 
+   */
+  ngOnInit(): void{
+    //Get Passed product id
+    this.activatedRoute.params.subscribe(params => {
+      // Initialize product id property with the passed ID
+      this.productId = +params['id']
+    })
+
+    // call the helper function from the service to get product using a product id
+    this.productService.getProductByID(this.productId).subscribe({
+      next: (data) => {
+        // after matching, initialize product property
+        this.product = data  
+      },
+      error: (error) => {
+        //print out any error might occur
+        console.log(error);
+      }
+    })
+  }
+  
+   /**
+   * @description Helper function that redirects user to edit product component
+   */
+   editProduct():void{
+    this.router.navigate(['/editproduct', this.productId])
+  }
+
+   /**
+   *@description Helper function that remove product from fakestore api 
+  */
+ removeProduct():void{
+  // call the helper function from product service to remove product from fakestore api
+  this.productService.removeProduct(this.productId).subscribe({
+    next: response => {
+      console.log(response);
+      
+       // show success message
+      this.showSuccessMessage = true
+      // set timeout to navigate to products component after product has been removed successfully 
+      setTimeout(() => {
+        // navigate to products component
+        this.router.navigate(['/products'])
+      },2000)
+    },
+    error: error => {
+      console.log(error);
+
+      // show error message
+      this.showFailedMessage = true
+      // set timeout to display error message 
+      setTimeout(() => {
+        // hide error message
+        this.showFailedMessage = false
+      },1500)
+    }
+  }) 
+ }
+
 
 }
