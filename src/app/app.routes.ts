65d2f5e5a1f24823a77bcff8d01251c3c6ba30a5
Mncedisi Masondo <mncedisimasondo4@gmail.com>
2024-11-10 01:22:49 +0200

    added page not found component and modified app route file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,14 +5,16 @@ import { AddProductComponent } from './components/add-product/add-product.compon
 import { EditProductComponent } from './components/edit-product/edit-product.component';
 import { UsersComponent } from './components/users/users.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
 export const routes: Routes = [
 
     {path:'login', component: SignInComponent},
-    {path:'products', component: IndexComponent},
+    {path:'index', component: IndexComponent},
     {path:'productdetail', component: ProductDetailComponent},
     {path:'addproduct', component: AddProductComponent},
     {path:'editproduct', component: EditProductComponent},
     {path:'users', component:UsersComponent},
-    {path:'', redirectTo:'/login', pathMatch:'full'}
+    {path:'', redirectTo:'/login', pathMatch:'full'},
+    {path:'**', component:PagenotfoundComponent}
 ];
