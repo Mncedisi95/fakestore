ec30897175e8baa125243e61ab976e513bf2c017
Mncedisi Masondo <mncedisimasondo4@gmail.com>
2024-11-10 13:34:26 +0200

    modified product service -added fliter functionality.

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -85,5 +85,45 @@ export class ProductService {
     // Send HTTP DELETE request
     return this.httpClient.delete<any>(this.apiurl + '/' + productId)
   }
+
+  /**
+   * @method getJeweleries
+   * @description helper function that get all products under juwelery category
+   * @returns {products} list of products under jewelery category
+   */
+  getJeweleries(): Observable<any>{
+    // Send HTTP GET request
+     return this.httpClient.get<any>(this.apiurl + '/category/jewelery')
+  }
+
+  /**
+   * @method getElectronics
+   * @description helper function that list of products under electronics category
+   * @returns {products} The list of products under elelctronics category
+   */
+  getElectronics(): Observable<any>{
+    //send HTTP GET request 
+    return this.httpClient.get<any>(this.apiurl + '/category/electronics')
+  }
+
+  /**
+   * @method
+   * @description helper function that get list of products under men's clothing category
+   * @returns {products} - The list of products under men's category
+   */
+  getMenClothing(): Observable<any>{
+    // send HTTP GET request 
+    return this.httpClient.get<any>(this.apiurl + '/category/mens clothings')
+  }
+
+  /**
+   * @method getWomenClothing
+   * @description Helper function that get list of products under women's clothing category
+   * @returns {products} The list of products under women's clothing category
+   */
+  getWomenClothing(): Observable<any>{
+    // send HTTP GET request
+    return this.httpClient.get<any>(this.apiurl + '/category/womens clothing')
+  }
  
 }
