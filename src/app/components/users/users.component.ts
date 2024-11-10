1908844495bd2fafa48682cf04ea8821076a0b08
Mncedisi Masondo <mncedisimasondo4@gmail.com>
2024-11-10 02:11:38 +0200

    modified user component and user service.

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,12 +1,45 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { UserService } from '../../services/user.service';
+
+/**
+ * @author Mncedisi Masondo
+ * 
+ * @description Component Responsible for displaying all users from fakestore api
+ * 
+ */
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
+
+ /**
+ * @property users
+ */
+  users : any[] = []
+
+  /**
+  * @constructor
+  * @param userService 
+  */
+  constructor(private userService:UserService){}
+  
+  /**
+  * @description initializes the component after angular fist display the data-bound properties
+  * Fetches products data from the fakestore api 
+  */
+  ngOnInit(): void{
+    // call the helper function from user service to fetch all users from fakestore api
+    this.userService.getUsers().subscribe(data => {
+       // initialize users property with data from user service
+       this.users = data     
+    })
+
+  }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+/**
+ * @author Mncedisi Masondo
+ * 
+ * @description Service Responsible for Fecting all users from fakestore api
+ */
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  /**
+   * @property dataurl - api link
+   */
+  private dataurl = 'https://fakestoreapi.com/users'
+
+  /**
+   * @constructor
+   * @param httpClient 
+   */
+  constructor(private httpClient:HttpClient) { }
+
+  /**
+   * @description helper function that get all users from fakestore api
+   * 
+   * @returns the list of users
+   */
+  getUsers(): Observable<any>{
+    // send HTTP GET request
+    return this.httpClient.get<any>(this.dataurl)
+  }
+
 }
